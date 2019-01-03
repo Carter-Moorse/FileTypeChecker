7c8308170ddd1b45b44bdc5b89e898042af3445d
WardBrink <brinkward@gmail.com>
2019-01-03 10:11:17 +0100

    Updated name, added Apache Tika libraries

--- javasource/system/UserActionsRegistrar.java
+++ javasource/system/UserActionsRegistrar.java
@@ -12,6 +12,7 @@ public class UserActionsRegistrar
   public void registerActions(IActionRegistrator registrator)
   {
     registrator.bundleComponentLoaded();
+    registrator.registerUserAction(fileidentifier.actions.GetMimeType.class);
     registrator.registerUserAction(system.actions.VerifyPassword.class);
   }
 }
